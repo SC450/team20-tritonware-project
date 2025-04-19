01161c7cf0f5760cb2905706bb352cfff19900e1
Justin Lu <45515570+skyflaren@users.noreply.github.com>
2025-04-18 23:11:09 -0700

    Add persistent wind ambient audio

--- Team 20 TritonWare Project/Assets/LevelSelect.cs
+++ Team 20 TritonWare Project/Assets/LevelSelect.cs
@@ -6,5 +6,12 @@ public class LevelSelect : MonoBehaviour
     public void LevelLoad (int level)
     {
         SceneManager.LoadScene(level+1);
+        GameObject.FindGameObjectWithTag("Music").GetComponent<Ambience>().PlayMusic();
+    }
+
+    public void ReturnMainMenu ()
+    {
+        SceneManager.LoadScene(0);
+        GameObject.FindGameObjectWithTag("Music").GetComponent<Ambience>().PlayMusic();
     }
 }
